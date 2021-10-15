415edbf869967ff5889619865be565c1330b8b6b
Felipe Cerquera <andrescercal@hotmail.com>
2021-10-15 17:07:15 -0500

    first commit

---
+++ scripts/my_imu.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import serial
+import sys
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Quaternion
+import rospy
+port = '/dev/ttyACM0'
+
+ser = serial.Serial(port, 115200, timeout=1.0)
+data = []
+my_imu = Imu()
+my_imu.header.frame_id = "imu_link"
+my_imu.orientation_covariance = [0.001, 0.0, 0.0, 0.0, 0.001, 0.0, 0.0, 0.0, 0.001]
+my_imu.angular_velocity_covariance = [0.001, 0.0, 0.0, 0.0, 0.001, 0.0, 0.0, 0.0, 0.001]
+my_imu.linear_acceleration_covariance = [0.001, 0.0, 0.0, 0.0, 0.001, 0.0, 0.0, 0.0, 0.001]
+
+imu_pub = rospy.Publisher("my_imu_topic", Imu, queue_size=10)
+
+rospy.init_node("imu_node", anonymous=False)
+
+def read_data():
+	error = True
+	vec = []
+	while (not(len(vec) >= 6 and error == False)):
+		#print("En ciclo de lectura")
+		try:
+			msg = ser.readline()	
+			msg_sensor = msg.decode('ascii')
+			vec = msg_sensor.split(",")
+			vec.pop()
+			#print(vec)
+			error = False
+		except UnicodeDecodeError:
+			error = True
+	return vec
+
+while True:
+	try:
+		data = read_data()
+		my_imu.orientation.x = 0
+		my_imu.orientation.y = 0
+		my_imu.orientation.z = 0
+		my_imu.orientation.w = 1
+		my_imu.angular_velocity.x = float(data[3])
+		my_imu.angular_velocity.y = float(data[4])
+		my_imu.angular_velocity.z = float(data[5])
+		my_imu.linear_acceleration.x = float(data[0])
+		my_imu.linear_acceleration.y = float(data[1])
+		my_imu.linear_acceleration.z = float(data[2])
+		my_imu.header.stamp = rospy.Time.now()
+		imu_pub.publish(my_imu)
+	except ValueError:
+		continue
+
+
---
+++ src/my_imu_from_odom.cpp
@@ -0,0 +1,46 @@
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/AccelWithCovarianceStamped.h>
+#include "ros/ros.h"
+
+
+using namespace sensor_msgs;
+using namespace message_filters;
+
+ros::Publisher my_imu_from_odom_pub;
+
+sensor_msgs::Imu my_imu_odom;
+
+void callback(const nav_msgs::Odometry::ConstPtr& odometry, const geometry_msgs::AccelWithCovarianceStamped::ConstPtr& accel)
+{
+  my_imu_odom.header = odometry->header;
+  my_imu_odom.orientation = odometry->pose.pose.orientation;
+  my_imu_odom.angular_velocity = odometry->twist.twist.angular;
+  my_imu_odom.linear_acceleration = accel->accel.accel.linear;
+
+  my_imu_from_odom_pub.publish(my_imu_odom); 
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "my_imu_from_odom_node");
+
+  ros::NodeHandle nh;
+  
+  my_imu_odom.orientation_covariance = {1,0,0,0,1,0,0,0,1};
+  my_imu_odom.angular_velocity_covariance = {1,0,0,0,1,0,0,0,1};
+  my_imu_odom.linear_acceleration_covariance = {1,0,0,0,1,0,0,0,1};
+  
+  my_imu_from_odom_pub = nh.advertise<Imu>("my_imu_from_odom", 10);
+
+  message_filters::Subscriber<nav_msgs::Odometry> odometry_sub(nh, "odometry/filtered/my_imu", 1);
+  message_filters::Subscriber<geometry_msgs::AccelWithCovarianceStamped> accel_sub(nh, "accel/filtered/my_imu", 1);
+  TimeSynchronizer<nav_msgs::Odometry, geometry_msgs::AccelWithCovarianceStamped> sync(odometry_sub, accel_sub, 10);
+  sync.registerCallback(boost::bind(&callback, _1, _2));
+
+  ros::spin();
+
+  return 0;
+}
